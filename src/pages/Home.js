7e81f815adf26bbcb4fc1db5dd77b6a4abcd0752
Amitkumar Yadav (Capgemini America Inc) <v-amiyadav@microsoft.com>
2023-08-09 14:16:11 +0530

    Part1 - Home Page Template Added

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../features/navbar/Navbar';
+import ProductList from '../features/Product List/ProductList';
+
+function Home() {
+  return (
+    <div>
+      <Navbar>
+        <ProductList>
+        </ProductList>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Home
